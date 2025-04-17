1027e22bfeefbf285cb390e34388eaee8221f2ca
Jiri Urbasek <jiri@urbasek.cz>
2025-04-17 12:45:21 +0200

    Cleanup of UDPPingsSequenceTests; Use helper `expect(...)` method to increase simplicity and readability of test cases; Assert also for clock time when a ping update was received

--- RMBTTests/NetworkCoverage/Pings/UDPPingsSequenceTests.swift
+++ RMBTTests/NetworkCoverage/Pings/UDPPingsSequenceTests.swift
@@ -9,149 +9,110 @@
 import Testing
 import Foundation
 @testable import RMBT
-import Network
 import Clocks
 
 struct UDPPingsSequenceTests {
     @Test func whenInitializingPingSession_thenDoesNotReportAnyPings() async throws {
-        let pingResults = [makePingResult(.ms(100))]
         let clock = TestClock()
-        let frequency: Duration = .milliseconds(500)
-        let sut = makeSUT(
-            clock: clock,
-            firstInitializationDate: Date(timeIntervalSinceReferenceDate: 0),
-            pingsFrequency: frequency,
-            initiatePingSessionDelays: [("1", .seconds(1.7))],
-            sendPingResults: pingResults
+        try await expect(
+            makeSUT(
+                clock: clock,
+                pingsFrequency: .milliseconds(500),
+                initiatePingSessionDelays: [("1", .seconds(1.7))],
+                sendPingResults: [
+                   .ms(100)
+                ]
+            ),
+            receive: [
+                (at: 2.1, makePingUpdate(ms: 100, startedAt: 2.0))
+            ],
+            after: .seconds(2.1),
+            with: clock
         )
-        var capturedElements: [PingResult] = []
-        await confirmation(expectedCount: pingResults.count) { confirmation in
-            Task {
-                for try await element in sut {
-                    capturedElements.append(element)
-                    confirmation.confirm()
-                }
-            }
-
-            for _ in 0 ... pingResults.count + 3 {
-                await clock.advance(by: frequency)
-            }
-        }
-
-        #expect(capturedElements.isEqual(to: [makePingResult(ms: 100, at: 2.0)]))
     }
 
     @Test func whenReceivingPingsShorterThenPingsFrequency_thenTheirTimestampDifferenceIsTheFrequency() async throws {
         let clock = TestClock()
-        let pingResults = [
-            makePingResult(.ms(50)),
-            makePingResult(.ms(20)),
-            makePingResult(.ms(120)),
-            makePingResult(.ms(40)),
-            makePingResult(.ms(250))
-        ]
-        let frequency = Duration.seconds(0.5)
-        let sut = makeSUT(
-            clock: clock,
-            firstInitializationDate: Date(timeIntervalSinceReferenceDate: 0),
-            pingsFrequency: frequency,
-            initiatePingSessionDelays: [("1", .seconds(0.2))],
-            sendPingResults: pingResults
+        try await expect(
+            makeSUT(
+                clock: clock,
+                pingsFrequency: .seconds(0.5),
+                initiatePingSessionDelays: [("1", .seconds(0.2))],
+                sendPingResults: [
+                    .ms(50),
+                    .ms(20),
+                    .ms(120),
+                    .ms(40),
+                    .ms(250)
+                ]
+            ),
+            receive: [
+                (at: 0.55, makePingUpdate(ms:  50, startedAt: 0.5)),
+                (at: 1.02, makePingUpdate(ms:  20, startedAt: 1)),
+                (at: 1.62, makePingUpdate(ms: 120, startedAt: 1.5)),
+                (at: 2.04, makePingUpdate(ms:  40, startedAt: 2.0)),
+                (at: 2.75, makePingUpdate(ms: 250, startedAt: 2.5))
+            ],
+            after: .seconds(2.75),
+            with: clock
         )
-        
-        var capturedElements: [PingResult] = []
-        await confirmation(expectedCount: pingResults.count) { confirmation in
-            Task {
-                for try await element in sut {
-                    capturedElements.append(element)
-                    confirmation.confirm()
-                }
-            }
-
-            for _ in 0 ... pingResults.count {
-                await clock.advance(by: frequency)
-            }
-        }
-
-        #expect(capturedElements == [
-            makePingResult(ms: 50, at: 0.5),
-            makePingResult(ms: 20, at: 1),
-            makePingResult(ms: 120, at: 1.5),
-            makePingResult(ms: 40, at: 2.0),
-            makePingResult(ms: 250, at: 2.5)
-        ])
     }
 
     @Test func whenReceivingPingsLongerThenPingsFrequency_thenTheirTimestampDifferenceIsTheFrequency() async throws {
         let clock = TestClock()
-        let pingResults = [
-            makePingResult(.ms(300)),
-            makePingResult(.ms(1200)),
-            makePingResult(.ms(2700)),
-            makePingResult(.ms(1300)),
-            makePingResult(.ms(100)),
-            makePingResult(.ms(700)),
-            makePingResult(.ms(1400)),
-            makePingResult(.ms(3800)),
-            makePingResult(.ms(1400)),
-            makePingResult(.ms(100)),
-            makePingResult(.ms(600)),
-            makePingResult(.ms(400))
-        ]
-        let frequency = Duration.seconds(1)
-        let sut = makeSUT(
-            clock: clock,
-            firstInitializationDate: Date(timeIntervalSinceReferenceDate: 0),
-            pingsFrequency: frequency,
-            initiatePingSessionDelays: [("1", .seconds(1.1))],
-            sendPingResults: pingResults
+        try await expect(
+            makeSUT(
+                clock: clock,
+                pingsFrequency: .seconds(1),
+                initiatePingSessionDelays: [("1", .seconds(1.1))],
+                sendPingResults: [
+                    .ms(300),
+                    .ms(1200),
+                    .ms(2700),
+                    .ms(1300),
+                    .ms(100),
+                    .ms(700),
+                    .ms(1400),
+                    .ms(3800),
+                    .ms(1400),
+                    .ms(100),
+                    .ms(600),
+                    .ms(400)
+                ]
+            ),
+            receive: [
+                (at:  2.3, makePingUpdate(ms:  300, startedAt: 2)),
+                (at:  4.2, makePingUpdate(ms: 1200, startedAt: 3)),
+                (at:  6.1, makePingUpdate(ms:  100, startedAt: 6)),
+                (at:  6.3, makePingUpdate(ms: 1300, startedAt: 5)),
+                (at:  6.7, makePingUpdate(ms: 2700, startedAt: 4)),
+                (at:  7.7, makePingUpdate(ms:  700, startedAt: 7)),
+                (at:  9.4, makePingUpdate(ms: 1400, startedAt: 8)),
+                (at: 11.1, makePingUpdate(ms:  100, startedAt: 11)),
+                (at: 11.4, makePingUpdate(ms: 1400, startedAt: 10)),
+                (at: 12.6, makePingUpdate(ms:  600, startedAt: 12)),
+                (at: 12.8, makePingUpdate(ms: 3800, startedAt: 9)),
+                (at: 13.4, makePingUpdate(ms:  400, startedAt: 13))
+            ],
+            after: .seconds(13.4),
+            with: clock
         )
-        var capturedElements: [PingResult] = []
-        var count = 0
-        await confirmation(expectedCount: pingResults.count) { confirmation in
-            Task {
-                for try await element in sut {
-                    capturedElements.append(element)
-                    confirmation.confirm()
-                }
-            }
-
-            for _ in 0 ... pingResults.count + 5 {
-                await clock.advance(by: frequency)
-            }
-        }
-
-        #expect(capturedElements == [
-            makePingResult(ms:  300, at: 2),
-            makePingResult(ms: 1200, at: 3),
-            makePingResult(ms:  100, at: 6),
-            makePingResult(ms: 1300, at: 5),
-            makePingResult(ms: 2700, at: 4),
-            makePingResult(ms:  700, at: 7),
-            makePingResult(ms: 1400, at: 8),
-            makePingResult(ms:  100, at: 11),
-            makePingResult(ms: 1400, at: 10),
-            makePingResult(ms:  600, at: 12),
-            makePingResult(ms: 3800, at: 9),
-            makePingResult(ms:  400, at: 13)
-        ])
     }
 }
 
 extension UDPPingsSequenceTests {
     func makeSUT(
         clock: some Clock<Duration>,
-        firstInitializationDate: Date,
+        firstInitializationDate: Date = Date(timeIntervalSinceReferenceDate: 0),
         pingsFrequency: Duration,
         initiatePingSessionDelays: [(PingSenderStub.PingSession, Duration)],
-        sendPingResults: [PingSenderStub.SendPingResult]
+        sendPingResults: [PingResultType]
     ) -> some PingsAsyncSequence {
         let pingSender = PingSenderStub(
             clock: clock,
             initiatePingSessionDelays: initiatePingSessionDelays,
-            sendPingResults: sendPingResults
+            sendPingResults: sendPingResults.map(makePingResult)
         )
-
         let sut = PingMeasurementService.pings2(
             clock: clock,
             pingSender: pingSender,
@@ -160,6 +121,29 @@ extension UDPPingsSequenceTests {
         )
 
         return sut
+    }
+
+    func expect(
+        _ sut: some PingsAsyncSequence,
+        receive expectedElements: [(at: Double, PingResult)],
+        after totalDuration: Duration,
+        with clock: TestClock<Duration>
+    ) async throws {
+        var capturedElements: [PingResult] = []
+        var capturedInstants: [TestClock<Duration>.Instant] = []
+        await confirmation(expectedCount: expectedElements.count) { confirmation in
+            Task {
+                for try await element in sut {
+                    capturedInstants.append(clock.now)
+                    capturedElements.append(element)
+                    confirmation.confirm()
+                }
+            }
+            await clock.advance(by: totalDuration)
+        }
+
+        #expect(capturedElements == expectedElements.map(\.1))
+        #expect(capturedInstants.isEqual(to: expectedElements.map { TestClock.Instant(offset: Duration.seconds($0.0)) }))
     }
 
     final class PingSenderStub: PingSending {
@@ -216,7 +200,7 @@ extension UDPPingsSequenceTests {
         }
     }
 
-    func makePingResult(ms: Int, at timeInterval: TimeInterval) -> PingResult {
+    func makePingUpdate(ms: Int, startedAt timeInterval: TimeInterval) -> PingResult {
         .init(result: PingResult.Result.interval(.milliseconds(ms)), timestamp: Date(timeIntervalSinceReferenceDate: timeInterval))
     }
 }
@@ -234,153 +218,21 @@ extension PingResult: @retroactive CustomTestStringConvertible, @retroactive Cus
     }
 }
 
-extension PingResult {
-    /// Custom equality check because for purpose of the unit tests we want to use only limited precission on comparision of `timestamp`
-    func isEqual(to other: PingResult) -> Bool {
-        result.isEquual(to: other.result) &&
-        timestamp.timeIntervalSinceReferenceDate - other.timestamp.timeIntervalSinceReferenceDate < 0.1
-    }
-}
-
-extension PingResult.Result {
-    func isEquual(to other: PingResult.Result) -> Bool {
-        switch (self, other) {
-        case (.interval(let lhsInterval), .interval(let rhsInterval)):
-            return (lhsInterval - rhsInterval).absolute < .milliseconds(Double(lhsInterval.milliseconds) * 0.15)
-        case (.error, .error):
-            return true
-        default:
-            return false
-        }
-    }
-}
-
 extension Duration {
     var absolute: Self {
         self >= .zero ? self : .milliseconds(-self.milliseconds)
     }
 }
-extension [PingResult] {
-    func isEqual(to other: [PingResult]) -> Bool {
-        guard count == other.count else { return false }
-        return zip(self, other).allSatisfy { $0.isEqual(to: $1) }
+
+extension TestClock.Instant where Duration == Swift.Duration {
+    func isEqual(to other: Self) -> Bool {
+        self.duration(to: other).absolute < .nanoseconds(1)
     }
 }
 
-// MARK: - Attempt to implement custom "Immediate Clock" (not working properly yet)
-
-public final class ManualClock: Clock, @unchecked Sendable {
-    public var minimumResolution: Duration = .zero
-
-    public struct Instant: InstantProtocol {
-        var offset: Duration = .zero
-
-        public func advanced(by duration: Duration) -> ManualClock.Instant {
-            Instant(offset: offset + duration)
-        }
-
-        public func duration(to other: ManualClock.Instant) -> Duration {
-            other.offset - offset
-        }
-
-        public static func < (_ lhs: ManualClock.Instant, _ rhs: ManualClock.Instant) -> Bool {
-            lhs.offset < rhs.offset
-        }
-    }
-
-    struct WakeUp {
-        var when: Instant
-        var continuation: UnsafeContinuation<Void, Never>
-    }
-
-    public private(set) var now = Instant()
-
-    // General storage for the sleep points we want to wake-up for
-    // this could be optimized to be a more efficient data structure
-    // as well as enforced for generation stability for ordering
-    var wakeUps = [WakeUp]()
-
-    // adjusting now or the wake-ups can be done from different threads/tasks
-    // so they need to be treated as critical mutations
-    let lock = os_unfair_lock_t.allocate(capacity: 1)
-
-    private var willBeAdvancing = false
-
-    deinit {
-        lock.deallocate()
-    }
-
-    public func sleep(until deadline: Instant, tolerance: Duration? = nil) async throws {
-        // Enqueue a pending wake-up into the list such that when
-        try Task.checkCancellation()
-
-        Task {
-            guard !willBeAdvancing else { return }
-
-            let closestDeadline = self.wakeUps
-                .sorted { lhs, rhs in lhs.when < rhs.when }
-                .first?.when ?? deadline
-
-            advance(by: now.duration(to: closestDeadline))
-        }
-        return await withUnsafeContinuation {
-            if deadline <= now {
-                $0.resume()
-            } else {
-                os_unfair_lock_lock(lock)
-                wakeUps.append(WakeUp(when: deadline, continuation: $0))
-                os_unfair_lock_unlock(lock)
-            }
-        }
-    }
-
-    public func advance(by amount: Duration) {
-        // step the now forward and gather all of the pending
-        // wake-ups that are in need of execution
-        os_unfair_lock_lock(lock)
-        willBeAdvancing = true
-
-        let finalNow = now.advanced(by: amount)
-
-        var toService = [WakeUp]()
-        for index in (0..<(wakeUps.count)).reversed() {
-            let wakeUp = wakeUps[index]
-            if wakeUp.when <= finalNow {
-                toService.insert(wakeUp, at: 0)
-                wakeUps.remove(at: index)
-            }
-        }
-        os_unfair_lock_unlock(lock)
-
-        // make sure to service them outside of the lock
-        toService.sort { lhs, rhs -> Bool in
-            lhs.when < rhs.when
-        }
-        for item in toService {
-            os_unfair_lock_lock(lock)
-            now = item.when
-            os_unfair_lock_unlock(lock)
-            item.continuation.resume()
-        }
-
-        os_unfair_lock_lock(lock)
-        if now < finalNow {
-            now = finalNow
-        }
-
-        if !wakeUps.isEmpty {
-            willBeAdvancing = true
-
-            Task {
-                if let closestDeadline = (self.wakeUps
-                    .sorted { lhs, rhs in lhs.when < rhs.when }
-                    .first?.when) {
-                        advance(by: now.duration(to: closestDeadline))
-                    }
-            }
-        } else {
-            willBeAdvancing = false
-        }
-        os_unfair_lock_unlock(lock)
+extension [TestClock<Duration>.Instant] {
+    func isEqual(to other: Self) -> Bool {
+        guard count == other.count else { return false }
+        return zip(self, other).allSatisfy { $0.isEqual(to: $1) }
     }
 }
